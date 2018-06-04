14ece322cbc4a0c2219fa45840a1d357f504f188
Daniel Phiri <malgamves@gmail.com>
2018-06-04 18:15:55 +0530

    Trivial changes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sync-Daniel-Phiri
 Go Sync code task   
+
+Clone repo
+
+run `` npm install ``
+
+run `` node app.js ``
+
+*Make sure you have node.js installed*
--- app.js
+++ app.js
@@ -1,43 +1,54 @@
 const fs = require('fs');
+const chalk = require('chalk');
 const AWS = require('aws-sdk');
 const watch = require('watch');
 const chokidar = require('chokidar');
 const readline = require('readline');
 const debug = require('debug');
 
-
+// Initializing console interface with readline
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+var answer;
 
-rl.question('What file directory do you want to monitor?', (answer) => {
+rl.question('What file directory do you want to monitor?\n', (answer) => {
     // TODO: Log the answer in a database maybe?
-    console.log(`monitoring: ${answer}`);
+    console.log(`monitoring: ${chalk.blue(answer)}`);
+
+    // Not appearing in console
+    debug(`this is the path : ${chalk.blue(answer)}`);
 
     rl.close();
 });
 
-debug(answer);
+// Also not appreaing in console
+debug(`this is the path : ${chalk.blue(answer)}`);
 
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
+// FDirectory watcher for all platforms, initialization below
 var watcher = chokidar.watch(answer, {
     ignored: /(^|[\/\\])\../,
     persistent: true
 });
 
 var log = console.log.bind(console);
+
+// Array to story paths of changed files 
 var paths = [];
+
 // Add event listeners.
 watcher
   .on('add', path => log(`File ${path} has been added`) && paths.push(path))
   .on('error', error => log(`Watcher error: ${error}`));
 
+// For loop to upload each file changed to an s3 bucket  
 for (i = 0; i < paths.length ; i++) {
 
     const fileName = paths[i];
@@ -55,6 +66,6 @@ for (i = 0; i < paths.length ; i++) {
             });
         });
     };
-
+    // Uploads file
     uploadFile();
 }
